63ad1b3f7ea115a5ac4cca085176045ef01fc68c
Ankit Tiwari <ankittiwariiit@gmail.com>
2024-07-24 06:54:31 +0530

    2nd commit to handle sum of one or two integers

--- src/add.js
+++ src/add.js
@@ -4,7 +4,8 @@ function stringCalculator(expression){
     if (!expression || expression.length === 0){
         return 0;
     }
-
+    const splitExpression = expression.split(",");
+    return (parseInt(splitExpression[0]) + parseInt(splitExpression[1])) || parseInt(expression);
 }
 
 module.exports = {
--- src/add.test.js
+++ src/add.test.js
@@ -4,3 +4,13 @@ const calculator = require("./add");
 test(`Evaluates the output to 0 the input is empty string -> "" `, ()=>{
     expect(calculator.calc()).toEqual(0);
 })
+
+test("Evaluates input string of size 1 or 2 separated by comma", () => {
+    //If the expression is "10" it will return 10 as output and if the expression is "10,10"
+    //result will be 20.
+
+
+    var expression = "42"; //Taking any arbitrary value to test this case.
+    var result = 42;          //The sum of the numbers taken.
+    expect(calculator.calc(expression)).toEqual(result);
+})
